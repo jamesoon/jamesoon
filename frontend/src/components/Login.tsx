d69abbd069137d94a696a3422bc5d7d3f7e88a28
jamezoon <jamesooneh@yahoo.com>
2025-12-08 10:10:00 +0800

    Update frontend version to v1.08_github

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -152,7 +152,7 @@ const Login: React.FC = () => {
           </Box>
 
           <Typography variant="caption" color="text.secondary" sx={{ display: 'block', mt: 2, textAlign: 'center', opacity: 0.7 }}>
-            v1.07
+            v1.08_github
           </Typography>
         </Box>
       </Paper>
